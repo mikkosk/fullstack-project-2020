2705fd819eec186e8c973b37e30b98f19d52f6ac
Mikko Kivistö <mikkosakarikivisto@gmail.com>
2021-02-19 11:01:50 +0200

    update

--- backend/build/src/index.js
+++ backend/build/src/index.js
@@ -5,11 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = __importDefault(require("./app"));
 var http_1 = __importDefault(require("http"));
-var path_1 = __importDefault(require("path"));
 var server = http_1.default.createServer(app_1.default);
 var PORT = process.env.PORT || 3001;
 server.listen(PORT, function () {
-    console.log(path_1.default.join(__dirname, '../frontend/index.html'));
     console.log(process.env.NODE_ENV);
     console.log("Server running on port " + PORT);
 });
